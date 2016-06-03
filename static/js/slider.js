3f3597e3cba36a6ae4f694a68d7765c3c30eaf2a
yyhuaisha <2433273227@qq.com>
2016-06-03 16:48:41 +0800

    update

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,24 +1,57 @@
-$(function(){
-	var sw = 0;
-	function myShow(i){
-		$("#sliderBox dd").eq(i).stop(true,true).animate({opacity:"1"},600).css("z-index","3").siblings("dd").animate({opacity:"0"},600).css("z-index","2");
-	}
-	//滑入停止动画，滑出开始动画
-	$("#sliderBox").hover(function(){
-		if(myTime){
-		   clearInterval(myTime);
+var glide = new function(){
+	function $id(id){return document.getElementById(id);};
+	this.layerGlide=function(auto,oSlider,sSingleSize,second,fSpeed,point){
+		var oSliderLi = $id(oSlider).getElementsByTagName('dd');
+		var interval,timeout,oslideRange;
+		var time = 1; 
+		var speed = fSpeed 
+		var sum = oSliderLi.length;
+		var a=0;
+		var delay=second * 1000; 
+		var setValLeft=function(s){
+			return function(){
+				oslideRange = Math.abs(parseInt($id(oSlider).style[point]));
+				$id(oSlider).style[point] =-Math.floor(oslideRange+(parseInt(s*sSingleSize) - oslideRange)*speed) +'px';		
+				if(oslideRange==[(sSingleSize * s)]){
+					clearInterval(interval);
+					a=s;
+				}
+			}
+		};
+		var setValRight=function(s){
+			return function(){	 	
+				oslideRange = Math.abs(parseInt($id(oSlider).style[point]));							
+				$id(oSlider).style[point] =-Math.ceil(oslideRange+(parseInt(s*sSingleSize) - oslideRange)*speed) +'px';
+				if(oslideRange==[(sSingleSize * s)]){
+					clearInterval(interval);
+					a=s;
+				}
+			}
 		}
-	},function(){
-		myTime = setInterval(function(){
-		  myShow(sw)
-		  sw++;
-		  if(sw==$("#sliderBox dd").length){sw=0;}
-		} , 5000);
-	});
-	//自动开始
-	var myTime = setInterval(function(){
-	   myShow(sw)
-	   sw++;
-	   if(sw==$("#sliderBox dd").length){sw=0;}
-	} , 5000);
-})
+		
+		function autoGlide(){
+			clearTimeout(interval);
+			if(a==(parseInt(sum)-1)){
+				a=0;
+				interval = setInterval(setValLeft(a),time);
+				timeout = setTimeout(autoGlide,delay);
+			}else{
+				a++;
+				interval = setInterval(setValRight(a),time);	
+				timeout = setTimeout(autoGlide,delay);
+			}
+		}
+	
+		if(auto){timeout = setTimeout(autoGlide,delay);};
+	}
+}
+ 
+//调用语句
+glide.layerGlide(
+	true,         //设置是否自动滚动
+	'slider_show',   //焦点图片容器
+	336,          //设置滚动图片位移像素
+	5,			  //设置滚动时间2秒 
+	0.1,          //设置过渡滚动速度
+	'left'		  //设置滚动方向“向左”
+);
